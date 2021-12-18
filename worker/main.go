ae0a532707c12f6cf1f9d70e34acbf8161c43951
Dylan Johnson <dylanjohnson@vista.local>
2021-12-18 13:09:26 -0700

    working cron

--- worker/main.go
+++ worker/main.go
@@ -3,27 +3,28 @@ package main
 import (
 	"log"
 
-	app "github.com/helvellyn-io/chaos"
-
+	"github.com/helvellyn-io/chaos"
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
 )
 
 func main() {
-	// Create the client object just once per process
-	c, err := client.NewClient(client.Options{})
+	// The client and worker are heavyweight objects that should be created once per process.
+	c, err := client.NewClient(client.Options{
+		HostPort: client.DefaultHostPort,
+	})
 	if err != nil {
-		log.Fatalln("unable to create Temporal client", err)
+		log.Fatalln("Unable to create client", err)
 	}
 	defer c.Close()
-	// This worker hosts both Worker and Activity functions
-	w := worker.New(c, app.ChaosMonkey, worker.Options{})
-	w.RegisterWorkflow(app.DeletePodWorkflow)
-	w.RegisterActivity(app.DeletePod)
-	// Start listening to the Task Queue
+
+	w := worker.New(c, "cron", worker.Options{})
+
+	w.RegisterWorkflow(chaos.DelPodWorkflow)
+	w.RegisterActivity(chaos.DelPod)
+
 	err = w.Run(worker.InterruptCh())
 	if err != nil {
-		log.Fatalln("unable to start Worker", err)
+		log.Fatalln("Unable to start worker", err)
 	}
-
 }
--- workflow.go
+++ workflow.go
@@ -1,17 +1,63 @@
 package chaos
 
 import (
+	"context"
+	"fmt"
+	"os"
 	"time"
 
+	"github.com/helvellyn-io/chaos/src/client"
+	"go.temporal.io/sdk/activity"
 	"go.temporal.io/sdk/workflow"
 )
 
-func DeletePodWorkflow(ctx workflow.Context) (string, error) {
-	options := workflow.ActivityOptions{
-		StartToCloseTimeout: time.Second * 5,
+// CronResult is used to return data from one cron run to the next
+type CronResult struct {
+	RunTime time.Time
+}
+
+// SampleCronWorkflow executes on the given schedule
+// The schedule is provided when starting the Workflow
+func DelPodWorkflow(ctx workflow.Context) (*CronResult, error) {
+
+	workflow.GetLogger(ctx).Info("Cron workflow started.", "StartTime", workflow.Now(ctx))
+
+	ao := workflow.ActivityOptions{
+		StartToCloseTimeout: 10 * time.Second,
 	}
-	ctx = workflow.WithActivityOptions(ctx, options)
-	var result string
-	err := workflow.ExecuteActivity(ctx, DeletePod).Get(ctx, &result)
-	return result, err
+	ctx1 := workflow.WithActivityOptions(ctx, ao)
+
+	// Start from 0 for first cron job
+	lastRunTime := time.Time{}
+	// Check to see if there was a previous cron job
+	if workflow.HasLastCompletionResult(ctx) {
+		var lastResult CronResult
+		if err := workflow.GetLastCompletionResult(ctx, &lastResult); err == nil {
+			lastRunTime = lastResult.RunTime
+		}
+	}
+	thisRunTime := workflow.Now(ctx)
+
+	err := workflow.ExecuteActivity(ctx1, DelPod, lastRunTime, thisRunTime).Get(ctx, nil)
+	if err != nil {
+		// Cron job failed
+		// Next cron will still be scheduled by the Server
+		workflow.GetLogger(ctx).Error("Cron job failed.", "Error", err)
+		return nil, err
+	}
+
+	return &CronResult{RunTime: thisRunTime}, nil
+}
+
+// DoSomething is an Activity
+func DelPod(ctx context.Context, lastRunTime, thisRunTime time.Time) error {
+	activity.GetLogger(ctx).Info("Cron job running.", "lastRunTime_exclude", lastRunTime, "thisRunTime_include", thisRunTime)
+
+	r := client.DeletePod(client.GetPods())
+	if r == "" {
+		os.Exit(1)
+
+	}
+	fmt.Println(r)
+	return nil
 }
